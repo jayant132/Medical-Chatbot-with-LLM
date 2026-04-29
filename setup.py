41941069aa0080e0989e10339e0e309cc5d5b9d7
JaybhatiaPrimo <jayant.bhatia@primotech.com>
2026-04-29 17:05:11 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical_chatbot',
+    version='1.0.0',
+    author="Jayant",
+    author_email="[jbhatia132@gmail.com]",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+)
