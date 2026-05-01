08daba4993580a165f0b32a51f1f2cea54c165fc
JaybhatiaPrimo <jayant.bhatia@primotech.com>
2026-05-01 13:12:55 +0530

    python script updated

---
+++ store_index.py
@@ -0,0 +1,63 @@
+from dotenv import load_dotenv
+import os
+
+from src.helper import (
+    load_pdf_file,
+    filter_to_minimal_docs,
+    text_split,
+    download_hugging_face_embeddings
+)
+
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+
+if not PINECONE_API_KEY:
+    raise ValueError("PINECONE_API_KEY not found in .env")
+
+
+extracted_data = load_pdf_file(data='data/')
+filter_data = filter_to_minimal_docs(extracted_data)
+text_chunks = text_split(filter_data)
+
+print(f"Loaded {len(text_chunks)} chunks")
+
+
+embeddings = download_hugging_face_embeddings()
+
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "medical-chatbot"
+
+existing_indexes = [i.name for i in pc.list_indexes()]
+
+if index_name not in existing_indexes:
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        ),
+    )
+    print(f"Index created: {index_name}")
+else:
+    print(f"Index already exists: {index_name}")
+
+
+index = pc.Index(index_name)
+
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings,
+)
+
+print("Data stored in Pinecone successfully")
